32ac115e68033a05a47550114d16d161e3eded5f
Python Newbie <pynewb@gmail.com>
2017-02-01 20:00:02 -0500

    Player now uses metadata store

--- node-audio-player.js
+++ node-audio-player.js
@@ -2,42 +2,67 @@
 
 'use strict';
 
+const MUSIC_ROOT = '/Users/snichol/music';
+
 const express = require('express');
 const morgan = require('morgan');
-const db = require('./db');
-const musicdata = require('./musicdata');
+const mp3store = require('./mp3store');
 
 const app = express();
 
 app.use(morgan('combined'));
-app.use('/music', express.static('C:\\media2\\music'));
+app.use('/music', express.static(MUSIC_ROOT));
 app.use('/', express.static(__dirname + '/www'));
 
-db.put('foo1', 'bar1');
-db.put('foo2', 'bar2');
-
-db.get('foo1', (err, value) => {
-  if (err) {
-    console.error('Error ' + err + ' getting foo1');
-  } else {
-    console.log('Have foo1 ' + value);
-  }
-});
-
 app.get('/api/artists', (req, res) => {
-  res.send(musicdata.artists);
+  mp3store.getArtists((err, artists) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.send(artists);
+    }
+  });
 });
 
 app.get('/api/artists/:artist/albums', (req, res) => {
-  res.send(musicdata.artistAlbums[req.params.artist]);
+  mp3store.getArtistAlbums(req.params.artist, (err, albums) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.send(albums);
+    }
+  });
 });
 
 app.get('/api/artists/:artist/albums/:album/tracks', (req, res) => {
-  res.send(musicdata.albumTracks[req.params.artist + '/' + req.params.album]);
+  mp3store.getAlbumTracks(req.params.artist, req.params.album, (err, tracks) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      tracks.sort((t1, t2) => {
+        return t1.trackNumber - t2.trackNumber;
+      });
+      let trackNames = tracks.map((track) => {
+        return track.track;
+      });
+      res.send(trackNames);
+    }
+  });
 });
 
 app.get('/api/artists/:artist/albums/:album/tracks/:track/details', (req, res) => {
-  res.send(musicdata.trackDetails[req.params.artist + '/' + req.params.album + '/' + req.params.track]);
+  mp3store.getTrackDetails(req.params.artist, req.params.album, req.params.track, (err, details) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      details.path = details.path.startsWith(MUSIC_ROOT) ? details.path.substr(MUSIC_ROOT.length, details.path.length) : details.path;
+      res.send(details);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
